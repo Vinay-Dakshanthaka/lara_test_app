3939ec0cd43b120938aec05d5d1a639827b65fea
Vinay <vinayd098@gmail.com>
2024-06-18 11:38:17 +0530

    signup controller method

--- lara_test_app/routes/auth.js
+++ lara_test_app/routes/auth.js
@@ -1,47 +1,11 @@
 const express = require('express');
 const authRoute = express.Router();
-const bcrypt = require('bcrypt');
-const { User } = require('../models'); // Adjust the path as per your project structure
 
-const saltRounds = 15;
+
+const authController = require('../controllers/authController')
+
 
 // POST route for user signup
-authRoute.post('/signup', async (req, res) => {
-    try {
-        const { name, email, phone, password } = req.body;
-
-        // Generate customId (e.g., "LARA00001")
-        const lastUser = await User.findOne({
-            order: [['customId', 'DESC']] // Ordering by customId
-        });
-
-        let lastIdNumber = 0;
-        if (lastUser && lastUser.customId) {
-            const lastId = lastUser.customId;
-            lastIdNumber = parseInt(lastId.replace('LARA', ''), 10);
-        }
-
-        const newIdNumber = lastIdNumber + 1;
-        const newCustomId = `LARA${newIdNumber.toString().padStart(5, '0')}`;
-        console.log("User ID : ", newCustomId)
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-        // Create the user in the database
-        const newUser = await User.create({
-            user_id: newCustomId,
-            name,
-            email,
-            phoneNumber: phone,
-            password: hashedPassword,
-            role: 'STUDENT', 
-            
-        });
-
-        res.status(200).send({ message: "Signup success", user: newUser });
-    } catch (error) {
-        res.status(500).send({ message: error.message });
-    }
-});
+authRoute.post('/signup', authController.signup); 
 
 module.exports = authRoute;
